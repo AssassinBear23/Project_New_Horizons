4bf0cdc40d3f07295ad5eb4c31b3dda061f9eace
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-06-17 14:14:17 +0200

    fixing physics issues
    trying to fix an issue where the player started random bouncing

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
 public class PlayerDeath : MonoBehaviour
 {
+    public UnityEvent onDeadge;
+    public Rigidbody rb;
+
     private void Update()
     {
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
 
         if (/*pos.x < 0 || pos.x > 1 ||*/ pos.y < -0.05f || pos.y > 1.05f) GoDie();
+
+        Debug.Log("Inertia:" + rb.linearVelocity);
     }
     private void GoDie()
     {
         Debug.Log("Deadge");
+        onDeadge?.Invoke();
     }
 }

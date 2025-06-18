1f33ed38aa134a0d7aac357cea4b1f39ffb98fe8
Beer Huizer <beer.huizer@gmail.com>
2025-06-18 15:50:58 +0200

    Import of UISystem from previous project
    Imported a unitypackage for a UI system that I (Beer Huizer) made for a previous project.
    
    Edited the UIManager and a few of the button's functions (signatures) to better fit the use case of this project.
    
    Changelog:
     - Created GameManager and InputManager with reference holding and methods needed to make basic UI navigation and functionality possible
     - Created Helper file/class for static methods, adding QuitApplication() to it.
     - UIManager
       - Removed unused variables.
       - Added GoToPage(UIPage) to allow the designers to make going to other pages easier, instead of having to know index, the now just need to give the UIPage object.
       - UIManager no longer holds the allowPaused boolean, moved that to GameManager.
       - Method to go to the original Caller page thats stored in the currently active UIPage.
     - LoadSceneButton now has a ReloadCurrentScene function that can be used to reload the scene.
     - UIPage
       - now holds a reference to any possible origin callers. Used by UIManager for the method to go back to original caller.
       - Has #nullable enable
    
    #18
    #47

---
+++ Assets/02_Scripts/Utilities/Managers/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Managers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+        public InputManager InputManager { get; set; }
+        public UIManager UIManager { get; set; }
+
+        #region Variables
+        [Header("Game state options")]
+        /// <summary>
+        /// Whether or not the game can be paused.
+        /// </summary>
+        [Tooltip("Whether or not the game can be paused")]
+        public bool AllowPause { get; set; }
+
+
+        #endregion
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void ToggleAllowPause()
+        {
+            AllowPause = !AllowPause;
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            Debug.Log("Quiting Application");
+#endif
+            StaticMethods.QuitApplication();
+        }
+    }
+}
---
+++ Assets/02_Scripts/Utilities/Managers/StaticMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public partial class StaticMethods
+{
+    /// <summary>
+    /// Quits the application. If running in the Unity editor, stops playing. If running in a build, quits the application.
+    /// </summary>
+    public static void QuitApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

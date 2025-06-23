f05a55f54294b60d538f64269ecc9c17706eb980
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-06-23 16:01:29 +0200

    implementing bird mechanic
    bird now kills player, still working on taking player to top of screen

--- Assets/02_Scripts/TerrainControl/BirdController.cs
+++ Assets/02_Scripts/TerrainControl/BirdController.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
 public enum Directions { Clockwise, Counterclockwise }
+public enum OnBirdTouchEvent { MovesToTopOfScreen, KillsPlayer}
 public class BirdController : MonoBehaviour
 {
     public Directions moveDirection = Directions.Clockwise;
+    [SerializeField] private OnBirdTouchEvent onBirdTouchEvent = OnBirdTouchEvent.KillsPlayer;
     [SerializeField] private float movementSpeed = 10;
+
+    private bool isEnabled = true;
     private void FixedUpdate()
     {
+        if (!isEnabled) return;
+
         float direction = (moveDirection == Directions.Clockwise) ? 1 : -1;
         transform.localEulerAngles = new Vector3(
             transform.localEulerAngles.x,
             transform.localEulerAngles.y + movementSpeed * direction,
             transform.localEulerAngles.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player" && isEnabled)
+        {
+            Debug.Log("Player hit bird");
+
+            switch(onBirdTouchEvent)
+            {
+                case OnBirdTouchEvent.MovesToTopOfScreen:
+                    Debug.LogWarning("Not Yet Implemented dumbass");
+                    break;
+                case OnBirdTouchEvent.KillsPlayer:
+                    Debug.Log("Killing Player");
+                    collision.gameObject.GetComponent<PlayerDeath>().onDeadge?.Invoke();
+                    isEnabled = false;
+                    break;
+            }
+        }
     }
 }
